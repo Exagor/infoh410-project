8ef2334afb6d63f157b8759ec09c397acec853a8
Nicolas Guilbaud <nicolas.guilbaud@ulb.be>
2024-05-01 17:10:16 +0200

    adding csv

--- main.py
+++ main.py
@@ -11,14 +11,16 @@ q_table_file = "q_table.csv"
 
 q_table = dict()
 
-# try:
-#     # transform csv into dictionary of dictionaries
-#     with open(q_table_file, mode='w') as file:
-#         reader = csv.reader(file)
-#         q_table = {rows[0]: {rows[1]: rows[2]} for rows in reader}
-# except:
-#     pass
+try:
+    # transform csv into dictionary of dictionaries
+    with open(q_table_file, mode='w') as file:
+        reader = csv.reader(file)
+        q_table = {rows[0]: {rows[1]: rows[2]} for rows in reader}
+except:
+    pass
 
+for key,value in q_table.items():
+    print(len(key),value)
 
 # env = gym.make('CartPole-v1', render_mode="human")
 env = gym.make(
@@ -33,27 +35,24 @@ env.metadata['render_fps'] = 30
 
 bot = agent(EPSILON, env, DISCOUNT, LR, Q_table=q_table)
 
-# state = env.reset()
-# key = bot.convert_state(state)
-# print(key)
+state = env.reset()
+key = bot.convert_state(state)
 
-# start = time()
-# bot.train(1)
-# end = time()
-# print(len(bot.Q_table))
-# print("Time taken: ", end-start)
+start = time()
+bot.train(1)
+end = time()
+print(len(bot.Q_table))
+print("Time taken: ", end-start)
 
-# for key,value in bot.Q_table.items():
-#     print(len(key),value)
+for key,value in bot.Q_table.items():
+    print(len(key),value)
 
-# # transform dictionary of dictionaries into csv
-# with open(q_table_file, mode='w') as file:
-#     writer = csv.writer(file)
-#     for key, value in bot.Q_table.items():
-#         for key2, value2 in value.items():
-#             writer.writerow([key, key2, value2])
+# bot.Q_table = {'a':{0:5,1:6,2:7},'b':{2:10,0:8}}
 
+# transform dictionary of dictionaries into csv
 with open(q_table_file, mode='w') as file:
     writer = csv.writer(file)
-    for i,j in enumerate(range(10)):
-        writer.writerow((i,j+1,i+j))
+    for key, value in bot.Q_table.items():
+        for key2, value2 in value.items():
+            writer.writerow([key, key2, value2])
+
