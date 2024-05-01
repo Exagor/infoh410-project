5b83c7e4830f8422cb95d8dfa747057a7558f7bb
Exagor <alexandre.achten@gmail.com>
2024-05-01 15:59:08 +0200

    test [not working]

--- agent.py
+++ agent.py
@@ -13,12 +13,28 @@ class agent:
     - alpha (learning rate)
     """
     def __init__(self, epsilon, env, gamma=0.95, alpha=0.1):
-        self.epsilon = epsilon
-        self.gamma = gamma
-        self.alpha = alpha
+        self.epsilon = epsilon #epsilon greedy
+        self.gamma = gamma #discount factor
+        self.alpha = alpha #learning rate
         self.env = env
         self.Q_table = dict()
 
+    def convert_state(self, state):
+        """
+        Convert the state to a string
+
+        Parameters:
+        - state: the state
+
+        Returns:
+        - the state as a string
+        """
+        str_state = ""
+        for i in state:
+            for j in i:
+                str_state += str(i)+" "
+        return str_state
+    
     def get_action(self, state):
         """
         Get the action based on the state
@@ -33,7 +49,8 @@ class agent:
         if rng.random() < self.epsilon:
             return self.env.action_space.sample()
         else:
-            return max(self.Q_table[state], key=self.Q_table[state].get)
+            str_state = self.convert_state(state)
+            return max(self.Q_table[str_state])
         
     def update_Q_table(self, state, action, reward, next_state):
         """
@@ -46,17 +63,19 @@ class agent:
         - next_state: the next state
         - env: the environment
         """
+        str_state = self.convert_state(state)
+        str_next_state = self.convert_state(next_state)
         # Initialize the Q table if the state or next state is not in the Q table
-        if state not in self.Q_table:
-            self.Q_table[state] = dict()
-        if next_state not in self.Q_table:
-            self.Q_table[next_state] = dict()
-        if action not in self.Q_table[state]:
-            self.Q_table[state][action] = 0.0
-        if action not in self.Q_table[next_state]:
-            self.Q_table[next_state][action] = 0.0
+        if str_state not in self.Q_table:
+            self.Q_table[str_state] = dict()
+        if str_next_state not in self.Q_table:
+            self.Q_table[str_next_state] = dict()
+        if action not in self.Q_table[str_state]:
+            self.Q_table[str_state][action] = 0.0
+        if action not in self.Q_table[str_next_state]:
+            self.Q_table[str_next_state][action] = 0.0
         
-        self.Q_table[state][action] += self.alpha * (reward + self.gamma * max(self.Q_table[next_state].values()) - self.Q_table[state][action])
+        self.Q_table[str_state][action] += self.alpha * (reward + self.gamma * max(self.Q_table[str_next_state].values()) - self.Q_table[str_state][action])
 
     def train(self, episodes=1000):
         """
@@ -68,9 +87,9 @@ class agent:
         """
         for episode in range(episodes):
             self.state = self.env.reset()
-            self.play_a_game()
+            self.play_a_game(train=True)
     
-    def play_a_game(self):
+    def play_a_game(self, train=False):
         """
         Play a game
         """
@@ -78,7 +97,10 @@ class agent:
         while not done:
             action = self.get_action(self.state)
             next_state, reward, done, _, _ = self.env.step(action)
-            self.update_Q_table(self.state, action, reward, next_state)
+            print(self.state)
+            print(next_state)
+            if train:
+                self.update_Q_table(self.state, action, reward, next_state)
             self.state = next_state
 
     def __del__(self):
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import gymnasium as gym
 from agent import agent
 
-DISCOUNT = 0.95
-LR = 0.1
+DISCOUNT = 0.95 #gamma discount factor
+LR = 0.1 #learning rate
+EPSILON = 0.1 #epsilon greedy
 
 # env = gym.make('CartPole-v1', render_mode="human")
 env = gym.make("ALE/SpaceInvaders-v5", render_mode="human", obs_type="grayscale")
 # env = gym.make("LunarLander-v2", render_mode="human")
 
-bot = agent(0.1, env, DISCOUNT, LR)
-bot.train(1000)
+bot = agent(EPSILON, env, DISCOUNT, LR)
+bot.train(10)
