25a36de391880fea730e274d7ad51b0a9fddbbf2
Nicolas Guilbaud <nicolas.guilbaud@ulb.be>
2024-05-21 16:14:23 +0200

    adding relative mean 50

--- src/plots.py
+++ src/plots.py
@@ -7,12 +7,41 @@ df = pd.read_csv('score.csv')
 
 column = df.iloc[:, 0]
 
-# Plot the graph
-plt.plot(column.index, column.values)
-plt.xlabel('Run number')
-plt.ylabel('Score')
-plt.title('Graph of Score during training')
-plt.grid(True)
+def to_mean_vals(values, n = 50):
+    for i,_ in enumerate(values):
+        if i+n > len(values):
+            break
+        print(i, i+n)
+        yield float(sum(values[i:i+n]))/n
 
-plt.savefig("figures/score.png")
-plt.show()
+def plot_graph(indx, vals, xlabel, ylabel, title, filename):
+    plt.plot(indx, vals)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.grid(True)
+
+    plt.savefig(filename)
+    plt.show()
+
+rel_mean_50 = list(to_mean_vals(column.values))
+
+# Plot the graph
+plot_graph(
+    column.index, 
+    column.values,
+    'Run number', 
+    'Score', 
+    'Graph of Score during training', 
+    'figures/score.png'
+)
+
+
+plot_graph(
+    column.index[:-49],
+    rel_mean_50, 
+    'Run number', 
+    'Score mean of 50 runs', 
+    'Relative mean of score during training', 
+    'figures/mean_50.png'
+)
